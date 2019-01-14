6e8b38ac6bc41c927ecda0244d2625a21ae0b420
Michael Truss <badmoonrising31@gmail.com>
2019-01-14 12:12:07 -0600

    Create questionaire.js

---
+++ questionaire.js
@@ -0,0 +1,41 @@
+(function(){
+function Question(question, answers, correct) {
+    this.question = question;
+    this.answers = answers;
+    this.correct = correct;
+}
+
+Question.prototype.displayQuestion = function() {
+    console.log(this.question);
+    for (var i = 0; i < this.answers.length; i++); {
+      console.log(i + ' :' + this.answers[i];);
+    }
+}
+
+Question.prototype.checkAnswer = function(ans) {
+    if (ans === this.correct) {
+       console.log('Correct Answer!');
+    } else {
+       console.log('Wrong Answer! Try Again!');
+    }
+}
+
+var q1 = new Question('Is JavaScript the coolest programming language in the world?',
+         ['Yes', 'No'], 0);
+         
+var q2 = new Question('What is the name of this course\'s teacher?',
+         ['John', 'Michael', 'Jonas'], 2);
+         
+var q3 = new Question('What best describes coding?',
+         ['Boring', 'Hard', 'Fun', 'Tedious'], 3);
+         
+var questions = [q1, q2, q3];
+
+var n = Math.floor(Math.random() * questions.length);
+
+questions[n].displayQuestion();
+
+var answer = parseInt(prompt('Please select the correct answer.'));
+
+questions[n].checkAnswer(answer);
+})();
